3d497e4641256afebbe15737308669b7b158174c
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-03 23:13:53 +0300

    Added dotenv to settings.

--- backend/storage_project/settings.py
+++ backend/storage_project/settings.py
@@ -1,6 +1,11 @@
 from pathlib import Path
 from .settings_database import DATABASES
 from .settings_sessions import *
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iz@$aux^f_s6jjk6r7ou094=w30ffyl3+ft4hcyg7mwbr=z4_7'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/storage_project/settings_database.py
+++ backend/storage_project/settings_database.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'file_storage_db',
         'USER': 'postgres',
-        'PASSWORD': 'juehtw',
+        'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': '5432',
     }
