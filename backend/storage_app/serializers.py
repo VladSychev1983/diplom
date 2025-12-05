588a43ee26bb143ac7c32d761adb99b870ec5b8c
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-05 13:09:15 +0300

    Resolved password validator

--- backend/storage_app/serializers.py
+++ backend/storage_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+import re
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,15 +9,26 @@ class RegisterSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password' : {'write_only': True}
         }
-        def validate(self, data):
+
+    def validate(self, data):
             # validate here data['password']
             # username состоит и латинских букв и цифр, первый символ буква, длина от 4 до 20
             # email это email
             # пароль 6 символов 1 цифра 1 заглавная и 1 спец символ
-            if not data['password']:
-                raise serializers.ValidationError('Not found password')
-            return data
-        
-        def create(self, validated_data):
-            instance = User.objects.create(**validated_data)
-            return instance
+        password = data['password']
+        if not password:
+            raise serializers.ValidationError('Not found password')
+        if len(password) < 6:
+             raise serializers.ValidationError('Password to short')
+        if not re.search(r'\d', password):
+             raise serializers.ValidationError({'error':'Password must contain at least one number'})
+        if not re.search(r'[A-Z]', password):
+             raise serializers.ValidationError('Password must contain at least one uppercase letter')
+        if not re.search(r'[!@#$%^&*()_+\-=\[\]{};:\'",.<>/?`~]', password):
+             raise serializers.ValidationError('Password must contain at least one special symbol')
+        return data
+
+         
+    def create(self, validated_data):
+        instance = User.objects.create(**validated_data)
+        return instance
