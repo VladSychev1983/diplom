6eafcdd3a90817c3c88420e937ce9a8699c9f935
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-05 12:18:02 +0300

    Added serializer.py and RegisterView

---
+++ backend/storage_app/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username','password','first_name', 'last_name','email')
+        extra_kwargs = {
+            'password' : {'write_only': True}
+        }
+        def validate(self, data):
+            # validate here data['password']
+            # username состоит и латинских букв и цифр, первый символ буква, длина от 4 до 20
+            # email это email
+            # пароль 6 символов 1 цифра 1 заглавная и 1 спец символ
+            if not data['password']:
+                raise serializers.ValidationError('Not found password')
+            return data
+        
+        def create(self, validated_data):
+            instance = User.objects.create(**validated_data)
+            return instance
--- backend/storage_app/views.py
+++ backend/storage_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from rest_framework.permissions import AllowAny
+from .permissions import IsAdminUser, IsOwnerOrReadOnly
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.http import JsonResponse, FileResponse
+import os
+from django.conf import settings
+from .serializers import RegisterSerializer
 
-# Create your views here.
+# регистрация нового пользователя.
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            instance =serializer.save()
+            return JsonResponse({"User created": "ok"})
+        #username = request.data.get('name')
+        #print(f'{username}')
+        return JsonResponse({"post": "ok"})
+
