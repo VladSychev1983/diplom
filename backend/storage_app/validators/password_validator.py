7215c9d6bbe3467a06159913f34aa969eb92e1e7
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-05 16:44:18 +0300

    Some changes are in password validator

--- backend/storage_app/validators/password_validator.py
+++ backend/storage_app/validators/password_validator.py
@@ -5,7 +5,7 @@ def is_valid_password(password):
     if not password:
         raise serializers.ValidationError({'password_error':'Not found password'})
     if len(password) < 6:
-        raise serializers.ValidationError({'password_error':'Password to short'})
+        raise serializers.ValidationError({'password_error':'Password is too short'})
     if not re.search(r'\d', password):
         raise serializers.ValidationError({'password_error':'Password must contain at least one number'})
     if not re.search(r'[A-Z]', password):
