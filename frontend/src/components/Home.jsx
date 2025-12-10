1392c6279ff39e741f5184bd494277c224df7750
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-11 00:32:42 +0300

    The frontend react base stuctural was created

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home()  {
+return (
+<React.Fragment>
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the main content of your application.</p>
+    </div>
+</React.Fragment>
+);
+
+}
+
+export default Home;
