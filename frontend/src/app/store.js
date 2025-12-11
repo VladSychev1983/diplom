a4f64a507fe65fd22f5d88bda2730b577e0752e3
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-11 13:50:30 +0300

    The redux persist was added in frontend

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from '../reducers'; 
 
-export const store = configureStore({
-    reducer: rootReducer
-});
+// Configuration for redux-persist
+const persistConfig = {
+  key: 'root', // Key for the storage object
+  storage, // The storage type (localStorage)
+  // Whitelist specifies which slices of the state to persist
+  whitelist: ['counter'], // Only the 'counter' state will be saved
+  // blacklist: ['user'] // Use blacklist to ignore specific reducers
+};
+
+// Wrap your root reducer with persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+     reducer: persistedReducer
+ });
+const persistor = persistStore(store);
+
+export { store, persistor };
--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,12 +1,11 @@
 import { combineReducers } from 'redux';
 import counterReducer from './counterReducer';
 import todosReducer from './todosReducer';
-//import { counterSlice } from './counterSlice';
 
-const indexSlice = combineReducers({
+const rootReducer = combineReducers({
   counter: counterReducer, // The 'counter' key will be the state's key
   todos: todosReducer,      // The 'todos' key will be the state's key
 
 });
 
-export default indexSlice;
+export default rootReducer;
