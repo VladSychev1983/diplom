e69fecf16156800266174b1837a6167bb46ce93e
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-22 17:45:29 +0300

    The validator was added to login form.

--- backend/storage_app/validators/password_validator.py
+++ backend/storage_app/validators/password_validator.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 
 def is_valid_password(password):
     if not password:
-        raise serializers.ValidationError({'password_error':'Not found password'})
+        raise serializers.ValidationError({'password_error':'Пароль не найден'})
     if len(password) < 6:
-        raise serializers.ValidationError({'password_error':'Password is too short'})
+        raise serializers.ValidationError({'password_error':'Пароль слишком короткий.  Должен быть менее 6 символов: как минимум одна заглавная буква, одна цифра и один специальный символ'})
     if not re.search(r'\d', password):
-        raise serializers.ValidationError({'password_error':'Password must contain at least one number'})
+        raise serializers.ValidationError({'password_error':'Пароль должен содержать минимум 1 цифру'})
     if not re.search(r'[A-Z]', password):
-        raise serializers.ValidationError({'password_error':'Password must contain at least one uppercase letter'})   
+        raise serializers.ValidationError({'password_error':'Пароль должен содержать минимум 1 букву в верхнем регистре'})   
     if not re.search(r'[!@#$%^&*()_+\-=\[\]{};:\'",.<>/?`~]', password):
-        raise serializers.ValidationError({'password_error': 'Password must contain at least one special symbol'})
+        raise serializers.ValidationError({'password_error': 'Пароль должен содержать минимум 1 спец. символ'})
     return password
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import LoginHandler
- from "./LoginHandler";
+import LoginHandler from "./LoginHandler";
+import { validateLogin } from "../Validators/valdiateLogin";
+import { validatePassword } from "../Validators/valdiatePassword";
  
 function Login() {
+    const [errLogin, setErrorLogin] = useState(null);
+    const [errPassword, setErrorPassword] = useState(null);
     const [formData, setFormData] = useState({
       username: '',
       password: '',
@@ -13,11 +16,29 @@ function Login() {
   
     const handlerChange = (e) => {
       //записываем в состояние данные формы.
+      setErrorLogin(false)
+      setErrorPassword(false)
       setFormData({ ...formData, [e.target.name]: e.target.value })
     }
   
     const handlerSubmit = (e) => {
       e.preventDefault();
+      const username = e.target["username"].value;
+      const password = e.target["password"].value;
+      const isValidLogin = validateLogin(username)
+      const isValidPass = validatePassword(password)
+
+      if(isValidLogin.status == false) {
+        setErrorLogin(isValidLogin.message);
+        return
+      }
+      if(isValidPass.status == false) {
+        setErrorPassword([isValidPass.message]);
+        return     
+      }
+      
+      console.log('send username:', password)
+      console.log('isValid:',isValidPass)
       //вызываем useEffect в RegisterComponent.
       setSendRequest(true)
     }
@@ -31,14 +52,18 @@ function Login() {
         <label htmlFor="username">Пользователь:</label>
         <input type="text" id="login_username" name="username" value={formData.username} onChange={handlerChange} required/>
         <br />
+        {errLogin}
         </div>
         <div  className="form_group">
         <label htmlFor="password">Пароль:</label>
-        <input type="password"  id="login_password" name="password" value={formData.password} onChange={handlerChange} />
+        <input type="password"  id="login_password" name="password" value={formData.password} onChange={handlerChange} required/>
         <br />
+        { errPassword ? errPassword.map((a) => <span key={errPassword.indexOf(a)}>{ a }</span>) : null }
+        {/* {errPassword} */}
         </div>
         <button type="submit" className='login_form_submit'>Войти</button>
       </form>
+
     </div>
         {/* {передаем данные формы и триггер в компонент обработчик RegisterHandler  } */}
     <LoginHandler formData={formData} sendRequest={sendRequest}
