9b6517217833e88981dca4bf346eb580a736d59b
Vladislav Sychev <vladsychev1983@gmail.com>
2025-12-22 14:44:22 +0300

    Some changes in home page.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,12 +6,17 @@ return (
 <React.Fragment>
     <div>
   <h1>Добро пожаловать в облачное хранилище файлов.</h1>
-  <div style={{textAlign:"center", fontSize:25}}>
+  <div style={{textAlign:"center", fontSize:25, lineHeight:2.5}}>
 Данное приложение позволяет сохранять файлы и обмениваться публичными ссылкам на загруженные файлы.
 <br />
 Пожалуйста,  <Link to='/register'>зарегистрируйтесь</Link> или  <Link to='/login'>авторизуйтесь.</Link>
 <br />
 Приятного использования и отличного настроения!
+</div>
+<div className="footer">
+  Все права защищены. <br />
+Разработано Sychev Vladislav  <br />
+  2025
 </div>
     </div>
     {/* <Counter /> */}
