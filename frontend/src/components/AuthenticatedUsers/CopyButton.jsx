0ed85df10fde17d84497abbeb495974d0ba94259
Vladislav Sychev <vladsychev1983@gmail.com>
2026-01-08 13:06:09 +0300

    Different deploy changes

--- frontend/src/components/AuthenticatedUsers/CopyButton.jsx
+++ frontend/src/components/AuthenticatedUsers/CopyButton.jsx
@@ -4,15 +4,32 @@ const CopyButton = ({ linkToCopy }) => {
   const [copied, setCopied] = useState(false);
 
   const handleCopy = async () => {
-    try {
+	if (window.isSecureContext && navigator.clipboard && typeof navigator.clipboard.writeText === 'function') {
+    		try {
       // Modern API for writing text to the clipboard
-      await navigator.clipboard.writeText(linkToCopy);
+      			await navigator.clipboard.writeText(linkToCopy);
       
       // Visual feedback
-      setCopied(true);
-      setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
-    } catch (err) {
-      console.error('Ошибка копирования: ', err);
+      			setCopied(true);
+      			setTimeout(() => setCopied(false), 2000); // Reset after 2 seconds
+    			} catch (err) {
+      				console.error('Ошибка копирования: ', err);
+    				} 
+	}
+   else {
+  const textArea = document.createElement("textarea");
+  textArea.value = linkToCopy;
+  document.body.appendChild(textArea);
+  textArea.select();
+	try {
+		document.execCommand('copy');
+		setCopied(true);
+		setTimeout(() => setCopied(false), 2000);
+
+	} catch (err) {
+		console.error('Fallback copy failed: ', err);
+	}
+   document.body.removeChild(textArea);
     }
   };
 
